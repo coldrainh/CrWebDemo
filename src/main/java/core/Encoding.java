81399b649560e776b3f8632034d94c44038f9493
coldrainh <dataqinsir@163.com>
2022-04-11 16:20:16 +0800

    Initial commit

---
+++ src/main/java/core/Encoding.java
@@ -0,0 +1,53 @@
+package core;
+
+import core.shell.ShellEntity;
+import util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class Encoding {
+    private static final String[] ENCODING_TYPES = new String[]{"UTF-8", "GBK", "GB2312", "BIG5", "GB18030", "ISO-8859-1"};
+    private final String charsetString;
+
+    private Encoding(String charsetString) {
+        this.charsetString = charsetString;
+    }
+
+    public static String[] getAllEncodingTypes() {
+        return ENCODING_TYPES;
+    }
+
+    public static Encoding getEncoding(ShellEntity entity) {
+        return new Encoding(entity.getEncoding());
+    }
+
+    public static Encoding getEncoding(String charsetString) {
+        return new Encoding(charsetString);
+    }
+
+    public byte[] Encoding(String string) {
+        try {
+            return string.getBytes(this.charsetString);
+        } catch (UnsupportedEncodingException var3) {
+            Log.error(var3);
+            return string.getBytes();
+        }
+    }
+
+    public String Decoding(byte[] srcData) {
+        if (srcData == null) {
+            return "";
+        } else if (Arrays.equals(srcData,"!@#$%^&*(".getBytes(StandardCharsets.UTF_8))) {
+          return "连接服务器超时！请重新再试！";
+        } else {
+            try {
+                return new String(srcData, this.charsetString);
+            } catch (UnsupportedEncodingException var3) {
+                Log.error(var3);
+                return new String(srcData);
+            }
+        }
+    }
+}
---
+++ src/main/java/core/ui/imp/ActionDbClick.java
@@ -0,0 +1,7 @@
+package core.ui.imp;
+
+import java.awt.event.MouseEvent;
+
+public interface ActionDbClick {
+    void dblClick(MouseEvent var1);
+}
